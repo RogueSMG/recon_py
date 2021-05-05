04eaf82d7874244bd4c8c2fdd0a78ac5a9efef4c
parthshah1 <parthshah14031998@gmail.com>
2021-05-05 14:40:44 -0700

    Update script.py

--- script.py
+++ script.py
@@ -4,21 +4,26 @@ import itertools
 from selenium import webdriver
 from time import sleep
 
-path = sys.argv[1]
-var = path.split('/')
-url =  var[0]
-del var[0]
-perm = list(itertools.permutations(var))
-res = [ '/'.join(x) for x in perm ]
-final_list = []
-for x in range(len(res)):
-	final_list.append(url+"/"+res[x])
-for y in range(len(final_list)):
-	req = requests.get("https://"+final_list[y])
-	if(req == '200'):
-		driver = webdriver.Firefox()
-		driver.get("https://"+final_list[y])
-		sleep(1)
-		driver.get_screenshot_as_file(final_list[y])
-		driver.quit()
+filename = sys.argv[1]
+
+with open(filename) as f:
+	lines = f.readlines()
+for x in range(len(lines)):
+	url_cut = lines[x].replace("https://www.","")
+	var = url_cut.split('/')
+	url =  var[0]
+	del var[0]
+	perm = list(itertools.permutations(var))
+	res = [ '/'.join(x) for x in perm ]
+	final_list = []
+	for x in range(len(res)):
+		final_list.append(url+"/"+res[x])
+	for y in range(len(final_list)):
+		req = requests.get("https://"+final_list[y])
+		if(req == '200'):
+			driver = webdriver.Firefox()
+			driver.get("https://"+final_list[y])
+			sleep(1)
+			driver.get_screenshot_as_file(final_list[y])
+			driver.quit()
 
